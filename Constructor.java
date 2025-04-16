056f0789f7c830f32c59e7fb2a455459af40ffd9
jayashree-jeyak <jayashreejeyak@gmail.com>
2025-04-16 20:53:19 +0530

    Add files via upload

---
+++ Constructor.java
@@ -0,0 +1,14 @@
+
+class Constructor{
+    int a;
+    String name;
+
+     Constructor() {
+        System.out.println("hello");
+    }
+    public static void main(String[]args)
+    {
+        Constructor ob1=new Constructor();
+    }
+    
+}
