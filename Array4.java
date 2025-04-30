5a064868ca0374bb462a2be0432a90fd7a40cbd4
jayashree-jeyak <jayashreejeyak@gmail.com>
2025-04-30 23:33:58 +0530

    Add files via upload

--- Array4.java
+++ Array4.java
@@ -1,29 +1,27 @@
-//get the input size of array
-// get input for each element in array
-import java.util.Scanner;
-import java.util.Arrays;
-class Array4{
-    public static void main (String [] args){
-       System.out.print("Enter the size of the array: ");
-        Scanner sc = new Scanner(System.in);
-        int a=sc.nextInt();
-        int[] array=new int[a];
-        System.out.println("enter the input:");
+class Array{
+    public static void main(String[] args) {
         
-       for(int i=0;i<a;i++){
-        array[i]=sc.nextInt();
-        
-       }
-       //System.out.println("the entered array is:"+Arrays.to string(Array));
-       System.out.println("The entered array is: " + Arrays.toString(array));
-        int c = a / 2;
-    if(a % 2 == 0){
-        System.out.println("mid is " + array[c-1]);
-    }
-    else{
-        System.out.println("mid is " + array[c]);
-
-    }
-   
+        //int biggest=a1[0];
+       
+        int a1[]={33,44,55,66};
+        int smallest=a1[0];
+        int biggest=a1[0];
+        for(int i=0;i<a1.length;i++){
+         
+         
+           int num=a1[i];
+         if(num>smallest){
+            smallest=num;
+         }
+         
+             //System.out.println(smallest);   
+             int n1=a1[i];    
+          if(n1<biggest){
+            n1=biggest;
+          }     
+        }
+       
+         System.out.println(smallest); 
+         System.out.println(biggest);
     }
 }
